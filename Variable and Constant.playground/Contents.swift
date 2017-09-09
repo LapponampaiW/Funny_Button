46a2b670f30e87e61c2f3a15646adf8737bfd72a
LapponampaiW <ballz_v@hotmail.com>
2017-09-09 11:53:49 +0700

    2

--- Variable and Constant.playground/Contents.swift
+++ Variable and Constant.playground/Contents.swift
@@ -3,3 +3,27 @@
 import UIKit
 
 var str = "Hello, playground"
+
+// การประกาศตัวแปร บน ภาษา Swift
+//Implicit
+
+var nameString = "Master UNG"
+
+nameString = "Doramon"
+
+//conStant
+let surnameString = "ABCD"
+//surnameString = "EFGH" // error เพราะ let แก้ไม่ได้
+
+//Print
+print("Name ==> \(nameString)")
+
+//about Integer
+let number1Int = 5
+let number2Int: Int = 10 // นี้คือการประกาศแบบ กำหนด DataType ว่า เป็น Int
+
+
+let answer = number1Int + number2Int
+print("answer ==> \(answer)")
+
+
