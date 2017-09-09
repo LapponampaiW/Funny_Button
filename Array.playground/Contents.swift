e4d8cf1dcf1088a436e5d39351d95fff3d99de5c
LapponampaiW <ballz_v@hotmail.com>
2017-09-09 14:37:24 +0700

    Array

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Array Format
+
+var numberAInts = [11,22,33,44,55]
+
+print("ขนาดของ Array \(numberAInts.count)")
+print("numberAInts[2] ==> \(numberAInts[2])")
+print("numberAInts[4] ==> \(numberAInts[4])")
+
+//Add Member
+numberAInts.append(66)
+numberAInts
+
+//Delete Member
+numberAInts.remove(at: 2)
+numberAInts
+
